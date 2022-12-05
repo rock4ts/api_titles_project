e9da1b5ce28f2ab0cbd369398cabff65a773b171
Artyom Sukhov <rocket.sukhov@gmail.com>
2022-12-05 22:20:04 +0300

    correct bad request

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,9 +11,9 @@ SECRET_KEY = os.getenv(
 DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = [
-    'localhost',
+    '158.160.8.237',
     '127.0.0.1',
-    '158.160.8.237'
+    'localhost'
 ]
 
 INSTALLED_APPS = [
