566492890b0fa0958d66991af91b0a7381a0ffa6
Artyom Sukhov <rocket.sukhov@gmail.com>
2023-02-09 08:04:35 +0300

    correct tests

--- tests/test_yamdb_yml.py
+++ tests/test_yamdb_yml.py
@@ -7,12 +7,12 @@ from .conftest import root_dir
 class TestWorkflow:
 
     def test_workflow(self):
-        yamdb_workflow_basename = 'yamdb_workflow'
+        titles_workflow_basename = 'titles_workflow'
 
-        yaml = f'{yamdb_workflow_basename}.yaml'
+        yaml = f'{titles_workflow_basename}.yaml'
         is_yaml = yaml in os.listdir(root_dir)
 
-        yml = f'{yamdb_workflow_basename}.yml'
+        yml = f'{titles_workflow_basename}.yml'
         is_yml = yml in os.listdir(root_dir)
 
         if not is_yaml and not is_yml:
@@ -24,7 +24,7 @@ class TestWorkflow:
 
         if is_yaml and is_yml:
             assert False, (
-                f'В каталоге {root_dir} не должно быть двух файлов {yamdb_workflow_basename} '
+                f'В каталоге {root_dir} не должно быть двух файлов {titles_workflow_basename} '
                 'с расширениями .yaml и .yml\n'
                 'Удалите один из них'
             )
@@ -33,18 +33,18 @@ class TestWorkflow:
 
         try:
             with open(f'{os.path.join(root_dir, filename)}', 'r') as f:
-                yamdb = f.read()
+                titles = f.read()
         except FileNotFoundError:
             assert False, f'Проверьте, что добавили файл {filename} в каталог {root_dir} для проверки'
 
         assert (
-                re.search(r'on:\s*push:\s*branches:\s*-\smaster', yamdb) or
-                'on: [push]' in yamdb or
-                'on: push' in yamdb
+                re.search(r'on:\s*push:\s*branches:\s*-\smaster', titles) or
+                'on: [push]' in titles or
+                'on: push' in titles
         ), f'Проверьте, что добавили действие при пуше в файл {filename}'
-        assert 'pytest' in yamdb, f'Проверьте, что добавили pytest в файл {filename}'
-        assert 'appleboy/ssh-action' in yamdb, f'Проверьте, что добавили деплой в файл {filename}'
-        assert 'appleboy/telegram-action' in yamdb, (
+        assert 'pytest' in titles, f'Проверьте, что добавили pytest в файл {filename}'
+        assert 'appleboy/ssh-action' in titles, f'Проверьте, что добавили деплой в файл {filename}'
+        assert 'appleboy/telegram-action' in titles, (
             'Проверьте, что настроили отправку telegram сообщения '
             f'в файл {filename}'
         )
